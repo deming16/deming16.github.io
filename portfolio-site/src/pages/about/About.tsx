67b765fb35e2a9cb0e659491105287ad50079719
Deming Lin <deming.lin@bytedance.com>
2024-12-04 10:22:06 +0800

    feat: about

--- portfolio-site/src/pages/about/About.tsx
+++ portfolio-site/src/pages/about/About.tsx
@@ -1,4 +1,7 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope, faGuitar } from "@fortawesome/free-solid-svg-icons";
 import SecondaryHeader from "../../components/SecondaryHeader";
+import TextButton from "../../components/TextButton";
 import profilePic from "../../img/about.jpg";
 
 const About = () => {
@@ -17,17 +20,17 @@ const About = () => {
             <div className="photo group-hover:scale-[.95]">
               <img
                 src={profilePic}
-                className="composition__photo--p2 w-[55%] rounded-[0.2rem] shadow-[0_1.5rem_4rem_rgba(0,0,0,0.4)] outline-offset-[2rem] transition-all duration-200 hover:z-20 hover:scale-105 hover:shadow-[0_2.5rem_4rem_rgba(0,0,0,0.5)] hover:outline-[1.5rem_solid_var(--color-primary)] sm:w-full sm:shadow-[0_1.5rem_3rem_rgba(0,0,0,0.2)]"
+                className="composition__photo--p2 w-[55%] rounded-[0.2rem] shadow-[0_1.5rem_4rem_rgba(0,0,0,0.4)] outline-offset-[2rem] transition-all duration-200 hover:z-20 hover:scale-105 hover:shadow-[0_2.5rem_4rem_rgba(0,0,0,0.5)] hover:outline-[1.5rem_solid_var(--color-primary)]"
               />
             </div>
           </div>
         </div>
 
         <div>
-          <h3 className="heading-tertiary u-margin-bottom-small wow fadeInUp">
+          <h3 className="heading-tertiary wow fadeInUp mb-6">
             Hi! My name is Deming
           </h3>
-          <p className="paragraph wow fadeInUp">
+          <p className="wow fadeInUp justify-center text-2xl">
             I am a software engineer passionate in delivering end-to-end web and
             cross platform software products. My interest includes software
             development, user experience, product management, technical
@@ -35,30 +38,30 @@ const About = () => {
           </p>
 
           <div className="wow fadeInUp">
-            <a
-              href="https://github.com/deming16"
-              target="_blank"
-              className="btn-text"
-            >
-              <i className="fab fa-github"></i>
-              Github
-            </a>
-            <a
-              href="https://www.linkedin.com/in/lindeming/"
-              target="_blank"
-              className="btn-text"
-            >
-              <i className="fab fa-linkedin"></i>
-              Linkedin
-            </a>
-            <a
-              href="mailto:deminglindm@gmail.com"
-              target="_blank"
-              className="btn-text"
-            >
-              <i className="fa fa-envelope"></i>
-              Email
-            </a>
+            <TextButton
+              href={"https://github.com/deming16"}
+              children={
+                <>
+                  <FontAwesomeIcon icon={faGuitar} /> Github
+                </>
+              }
+            />
+            <TextButton
+              href={"https://www.linkedin.com/in/lindeming/"}
+              children={
+                <>
+                  <FontAwesomeIcon icon={faGuitar} /> Linkedin
+                </>
+              }
+            />
+            <TextButton
+              href={"mailto:deminglindm@gmail.com"}
+              children={
+                <>
+                  <FontAwesomeIcon icon={faEnvelope} /> Email
+                </>
+              }
+            />
           </div>
         </div>
       </div>
