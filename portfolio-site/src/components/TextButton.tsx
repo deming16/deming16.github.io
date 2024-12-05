72fd6c1bd410a777dcd62d17e1d6b73c63f0e9a9
Deming Lin <deming.lin@bytedance.com>
2024-12-05 10:49:12 +0800

    feat: about

--- portfolio-site/src/components/TextButton.tsx
+++ portfolio-site/src/components/TextButton.tsx
@@ -10,7 +10,7 @@ const TextButton = ({ children, href }: Props) => {
     <a
       href={href}
       target="_blank"
-      className="btn-text inline-block border-b-[0.1rem] border-black py-[1rem] text-[1.6rem] text-black no-underline transition-all duration-200 hover:translate-y-[-2px] hover:bg-black hover:text-white hover:shadow-[0_1rem_2rem_rgba(0,0,0,0.15)] active:translate-y-0 active:shadow-[0_1rem_2rem_rgba(0,0,0,0.15)]"
+      className="btn-text inline-block border-b-[0.1rem] border-black p-[1rem] text-[1.6rem] text-black no-underline transition-all duration-200 hover:translate-y-[-2px] hover:bg-black hover:text-white hover:shadow-[0_1rem_2rem_rgba(0,0,0,0.15)] active:translate-y-0 active:shadow-[0_1rem_2rem_rgba(0,0,0,0.15)]"
     >
       {children}
     </a>
--- portfolio-site/src/pages/about/About.tsx
+++ portfolio-site/src/pages/about/About.tsx
@@ -7,7 +7,7 @@ import profilePic from "../../img/about.jpg";
 const About = () => {
   return (
     <section
-      className="mt-[-20vh] bg-[#f7f7f7] py-[25rem] lg:py-[25rem]"
+      className="mt-[-10vh] bg-[#f7f7f7] py-[25rem] lg:py-[25rem]"
       id="about"
     >
       <div className="lg:mb-[5rem]! wow fadeInUp mb-[8rem] text-center">
@@ -20,24 +20,21 @@ const About = () => {
             <div className="photo group-hover:scale-[.95]">
               <img
                 src={profilePic}
-                className="composition__photo--p2 w-[55%] rounded-[0.2rem] shadow-[0_1.5rem_4rem_rgba(0,0,0,0.4)] outline-offset-[2rem] transition-all duration-200 hover:z-20 hover:scale-105 hover:shadow-[0_2.5rem_4rem_rgba(0,0,0,0.5)] hover:outline-[1.5rem_solid_var(--color-primary)]"
+                className="composition__photo--p2 mx-auto w-[55%] rounded-[0.2rem] shadow-[0_1.5rem_4rem_rgba(0,0,0,0.4)] outline-offset-[2rem] transition-all duration-200 hover:z-20 hover:scale-105 hover:shadow-[0_2.5rem_4rem_rgba(0,0,0,0.5)] hover:outline-[1.5rem_solid_var(--color-primary)]"
               />
             </div>
           </div>
         </div>
 
         <div>
-          <h3 className="heading-tertiary wow fadeInUp mb-6">
-            Hi! My name is Deming
-          </h3>
-          <p className="wow fadeInUp justify-center text-2xl">
+          <p className="wow fadeInUp justify-center text-4xl">
             I am a software engineer passionate in delivering end-to-end web and
             cross platform software products. My interest includes software
             development, user experience, product management, technical
             leadership.
           </p>
 
-          <div className="wow fadeInUp">
+          <div className="wow fadeInUp mt-4 flex gap-6">
             <TextButton
               href={"https://github.com/deming16"}
               children={
