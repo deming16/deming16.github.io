b567c0c0ad46671740fdbd0a2f4424b7606c2812
deming16 <deminglindm@gmail.com>
2024-12-06 00:06:38 +0800

    refactor: delete old code

--- portfolio-site/src/scripts/navigation.js
+++
@@ -1,9 +0,0 @@
-var navItems = document.getElementsByClassName('navigation__item');
-
-Array.from(navItems).forEach((item) => {
-  item.addEventListener("click", toggleNavi);
-});
-
-function toggleNavi(e) {
-  document.getElementById("navi-toggle").checked = false;
-}
